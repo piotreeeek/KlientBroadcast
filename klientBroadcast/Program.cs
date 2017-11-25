7e264995536cfb581b51160c1a93733cfbf3da17
Piotrek <piotrek120017@gmail.com>
2017-11-26 00:03:38 +0100

    initial commit

---
+++ klientBroadcast/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace klientBroadcast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient udpClient = new UdpClient();
+
+            udpClient.ExclusiveAddressUse = false;
+            IPEndPoint localEp = new IPEndPoint(IPAddress.Any, 2222);
+            udpClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            udpClient.ExclusiveAddressUse = false;
+
+            udpClient.Client.Bind(localEp);
+
+            IPEndPoint remoteEp = new IPEndPoint(IPAddress.Any, 0);
+            string reciveMessage;
+            do
+            {
+                byte[] reciveByte = udpClient.Receive(ref remoteEp);
+                reciveMessage = Encoding.UTF8.GetString(reciveByte);
+                Console.WriteLine("Odebrano: " + reciveMessage);
+            } while (reciveMessage != String.Empty);
+            udpClient.Close();
+        }
+    }
+}
